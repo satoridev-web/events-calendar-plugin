31fed434ff8f966dcc97a5ad771360800597cd45
Andy Garard <andygarard@fusiond1.home>
2025-06-27 16:08:45 +1000

    Initial plugin file with header

---
+++ events-calendar-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Events Calendar Plugin
+Plugin URI: https://satori.com.au/
+Description: A lightweight events calendar with frontend submission support.
+Version: 1.0.0
+Author: Satori Graphics
+Author URI: https://satori.com.au/
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: events-calendar-plugin
+*/
